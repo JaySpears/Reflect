1efa7420a568a2a06db8a7d2bcc5d3fcef5fbc1b
JoshSpears3 <josh.spears@sq1.com>
2017-08-17 08:41:49 -0500

    untracked config

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'facebookAuth' : {
-        'clientID'      : '1745316892404909',
-        'clientSecret'  : 'bc94430d961f374516b9e9eb2399bd2c',
-        'callbackURL'   : '/auth/facebook/callback'
-    }
-};
